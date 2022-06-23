a2cbcc6aee4950b55b223621c7874c846fd05be5
pankcuf <rastacake@gmail.com>
2022-06-23 15:37:32 +0300

    [0.1.12: specify cbindgen]

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
 pub mod ffi;
 pub mod types;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+extern crate dash_spv_models;
