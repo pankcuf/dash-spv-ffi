3fcec61608de36b08f238f647a9c82c3fda9616d
pankcuf <rastacake@gmail.com>
2022-08-27 22:08:37 +0300

    [destroy_hash callback]

--- src/ffi/callbacks.rs
+++ src/ffi/callbacks.rs
@@ -23,46 +23,53 @@ pub type GetLLMQSnapshotByBlockHeight = unsafe extern "C" fn(block_height: u32, 
 pub type GetLLMQSnapshotByBlockHash = unsafe extern "C" fn(block_hash: *mut [u8; 32], context: *const c_void) -> *const types::LLMQSnapshot;
 pub type SaveLLMQSnapshot = unsafe extern "C" fn(block_hash: *mut [u8; 32], snapshot: *const types::LLMQSnapshot, context: *const c_void) -> bool;
 pub type LogMessage = unsafe extern "C" fn(message: *const libc::c_char, context: *const c_void);
+pub type HashDestroy = unsafe extern "C" fn(hash: *const u8);
+
+fn read_and_destroy_hash<DH>(lookup_result: *const u8, destroy_hash: DH) -> Option<UInt256>
+    where
+        DH: Fn(*const u8) {
+    if !lookup_result.is_null() {
+        let hash = UInt256::from_const(lookup_result);
+        if let Some(hash) = hash {
+            destroy_hash(hash.0.as_ptr());
+        }
+        hash
+    } else {
+        None
+    }
+}
 
 pub fn lookup_masternode_list<MNL, MND>(block_hash: UInt256, masternode_list_lookup: MNL, masternode_list_destroy: MND) -> Option<masternode::MasternodeList>
     where
         MNL: Fn(UInt256) -> *const types::MasternodeList + Copy,
         MND: Fn(*const types::MasternodeList) {
     let lookup_result = masternode_list_lookup(block_hash);
-    // println!("lookup_masternode_list___: {} {:#?}", block_hash, lookup_result);
     if !lookup_result.is_null() {
         let data = unsafe { (*lookup_result).decode() };
         masternode_list_destroy(lookup_result);
-        //println!("lookup_masternode_list (Some): {}", block_hash);
         Some(data)
     } else {
-        //println!("lookup_masternode_list (None): {}", block_hash);
         None
     }
 }
 
-pub fn lookup_block_hash_by_height<BL>(block_height: u32, lookup: BL) -> Option<UInt256>
-    where BL: Fn(u32) -> *const u8 + Copy {
-    let lookup_result = lookup(block_height);
-    if !lookup_result.is_null() {
-        UInt256::from_const(lookup_result)
-    } else {
-        None
-    }
+pub fn lookup_block_hash_by_height<BL, DH>(block_height: u32, lookup: BL, destroy_hash: DH) -> Option<UInt256>
+    where
+        BL: Fn(u32) -> *const u8 + Copy,
+        DH: Fn(*const u8) {
+    read_and_destroy_hash(lookup(block_height), destroy_hash)
 }
 
-pub fn lookup_merkle_root_by_hash<MRL>(block_hash: UInt256, lookup: MRL) -> Option<UInt256>
-    where MRL: Fn(UInt256) -> *const u8 + Copy {
-    let lookup_result = lookup(block_hash);
-    if !lookup_result.is_null() {
-        UInt256::from_const(lookup_result)
-    } else {
-        None
-    }
+pub fn lookup_merkle_root_by_hash<MRL, DH>(block_hash: UInt256, lookup: MRL, destroy_hash: DH) -> Option<UInt256>
+    where
+        MRL: Fn(UInt256) -> *const u8 + Copy,
+        DH: Fn(*const u8) {
+    read_and_destroy_hash(lookup(block_hash), destroy_hash)
 }
 
 pub fn lookup_snapshot<SL>(block_height: u32, snapshot_lookup: SL) -> Option<llmq::LLMQSnapshot>
-    where SL: Fn(u32) -> *const types::LLMQSnapshot + Copy {
+    where
+        SL: Fn(u32) -> *const types::LLMQSnapshot + Copy {
     let lookup_result = snapshot_lookup(block_height);
     if !lookup_result.is_null() {
         let data = unsafe { (*lookup_result).decode() };
@@ -73,7 +80,8 @@ pub fn lookup_snapshot<SL>(block_height: u32, snapshot_lookup: SL) -> Option<llm
 }
 
 pub fn lookup_snapshot_by_block_hash<SL>(block_hash: UInt256, snapshot_lookup: SL) -> Option<llmq::LLMQSnapshot>
-    where SL: Fn(UInt256) -> *const types::LLMQSnapshot + Copy {
+    where
+        SL: Fn(UInt256) -> *const types::LLMQSnapshot + Copy {
     let lookup_result = snapshot_lookup(block_hash);
     if !lookup_result.is_null() {
         let data = unsafe { (*lookup_result).decode() };
