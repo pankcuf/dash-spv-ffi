764e6be2835b62c170705195e25f72b33b24ab72
pankcuf <rastacake@gmail.com>
2022-10-17 12:22:24 +0400

    [check]

--- src/ffi/from.rs
+++ src/ffi/from.rs
@@ -349,15 +349,13 @@ impl FromFFI for types::LLMQSnapshot {
     type Item = snapshot::LLMQSnapshot;
 
     unsafe fn decode(&self) -> Self::Item {
-        let member_list_bytes =
-            slice::from_raw_parts::<u8>(self.member_list, self.member_list_length);
-        // let skip_list_bytes = slice::from_raw_parts::<i32>(self.skip_list, self.skip_list_length);
         Self::Item {
-            member_list: member_list_bytes.to_vec(),
-            skip_list: (0..self.skip_list_length)
-                .into_iter()
-                .map(|i| *(self.skip_list.offset(i as isize)))
-                .collect(),
+            member_list: slice::from_raw_parts(self.member_list, self.member_list_length).to_vec(),
+            skip_list: slice::from_raw_parts::<i32>(self.skip_list, self.skip_list_length).to_vec(),
+            // skip_list: (0..self.skip_list_length)
+            //     .into_iter()
+            //     .map(|i| *(self.skip_list.offset(i as isize)))
+            //     .collect(),
             skip_list_mode: self.skip_list_mode,
         }
     }
